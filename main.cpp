8bace09fd6ac878d1c69fcaade2e5751a4382fa1
Petcu Bogdan <bogdan.petcu02@e-uvt.ro>
2022-05-16 13:05:35 +0300

    meniu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,145 @@
 #include <iostream>
 using namespace std;
 
+
+void selecteaza_data_zi(int zile){
+    cout<<"0. Meniu principal\n\n";
+
+    cout<<"Alege o  zi:\n";
+
+    for(int i=1; i<=zile; i++ )
+        cout<<i<<" ";
+
+    int ver_zi;
+    cin>>ver_zi;
+    switch(ver_zi) {
+        case 0:
+            break;
+        default:
+            cout<<"Rezervarea a fost facuta cu succes.\n\n\n";
+            break;
+    }
+}
+
+void selecteaza_data_luna() {
+    cout<<"0. Meniu principal\n";
+    cout<<"1. Ianuarie\n";
+    cout<<"2. Februarie\n";
+    cout<<"3. Martie\n";
+    cout<<"4. Aprilie\n";
+    cout<<"5. Mai\n";
+    cout<<"6. Iunie\n";
+    cout<<"7. Iulie\n";
+    cout<<"8. August\n";
+    cout<<"9. Septenbrie\n";
+    cout<<"10. Octombrie\n";
+    cout<<"11. Noiembrie\n";
+    cout<<"12. Decembrie\n";
+    int ver;
+    cin>>ver;
+    switch(ver) {
+        case 0:
+            break;
+        case 1:
+            selecteaza_data_zi(31);
+            break;
+        case 2:
+            selecteaza_data_zi(28);
+            break;
+        case 3:
+            selecteaza_data_zi(31);
+            break;
+        case 4:
+            selecteaza_data_zi(30);
+            break;
+        case 5:
+            selecteaza_data_zi(31);
+            break;
+        case 6:
+            selecteaza_data_zi(30);
+            break;
+        case 7:
+            selecteaza_data_zi(31);
+            break;
+        case 8:
+            selecteaza_data_zi(31);
+            break;
+        case 9:
+            selecteaza_data_zi(30);
+            break;
+        case 10:
+            selecteaza_data_zi(31);
+            break;
+        case 11:
+            selecteaza_data_zi(30);
+            break;
+        case 12:
+            selecteaza_data_zi(31);
+            break;
+        default:
+            cout<<"Introduceti una din variantele existente\n\n\n";
+            break;
+    }
+}
+
+
+void selecteaza_sala() {
+    int ver;
+    cout<<"0. Meniu principal\n";
+    cout<<"1. Sala 1\n";
+    cout<<"2. Sala 2\n";
+    cout<<"3. Sala 3\n";
+    cout<<"4. Sala 4\n";
+    cin>>ver;
+    switch(ver) {
+        case 0:
+            break;
+        default:
+            selecteaza_data_luna();
+            break;
+    }
+}
+
+
+void creaza_rezervare() {
+    cout<<"Introduceti numele\n";
+    char nume[100];
+    cin>>nume;
+    int ver;
+    cout<<"0. Meniu principal\n";
+    cout<<"1. Selecteaza o sala\n";
+    cin>>ver;
+    switch(ver) {
+        case 0:
+            break;
+        case 1:
+            selecteaza_sala();
+            break;
+        default:
+            cout<<"Introduceti una din variantele existente\n\n\n";
+            break;
+    }
+}
+
+
+
+void afisare_sali() {
+    int ver;
+    cout<<"0. Meniu principal\n";
+    cout<<"1. Sala 1\n";
+    cout<<"2. Sala 2\n";
+    cout<<"3. Sala 3\n";
+    cout<<"4. Sala 4\n";
+    cout<<"\n\n";
+    cin>>ver;
+    switch(ver) {
+        case 0:
+            break;
+        default:
+            break;
+    }
+}
+
 int main(){
     bool run = true;
     int choice;
@@ -14,6 +153,7 @@ int main(){
         switch (choice) {
             case 1:
                 cout << "Se va crea o rezervare" << endl;
+                creaza_rezervare();
                 break;
             case 2:
                 cout << "Introduceti numele dvs: " << endl;
@@ -22,6 +162,7 @@ int main(){
                 break;
             case 4:
                 cout << "Printam sali" << endl;
+                afisare_sali();
                 break;
             case 5:
                 cout << "Printam rezervari" << endl;
